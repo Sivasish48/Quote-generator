078a80d4217db55c14a91dc26f0421869c8930a1
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2023-07-29 22:44:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('#new quote');
+
+const quote = document.querySelector('.quote');
+
+const person = document.querySelector('.person');
+
+const quotes = [{
+    quote: "To love oneself is the beginning of a lifelong romance." ,
+    person: "— Oscar Wilde"
+},
+{
+    quote: "Sometimes the most important thing in a whole day is the rest we take between two deep breaths." ,
+    person: "— Etty Hillesum"
+},
+{
+    quote:"" ,
+    person:""
+}]
