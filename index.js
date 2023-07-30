d061f9f37dd398992301939df111cc2a2ab7673b
Sivasish Praharaj <98220976+Sivasish48@users.noreply.github.com>
2023-07-30 22:32:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const btn = document.querySelector('#new quote');
+const btn = document.querySelector("#new-quote");
 
-const quote = document.querySelector('.quote');
+let quote = document.querySelector(".quote");
 
-const person = document.querySelector('.person');
+let person = document.querySelector(".person");
 
 const quotes = [{
     quote: "To love oneself is the beginning of a lifelong romance." ,
@@ -13,6 +13,48 @@ const quotes = [{
     person: "— Etty Hillesum"
 },
 {
-    quote:"" ,
-    person:""
-}]
+    quote:"Loved you yesterday, love you still, always have, always will." ,
+    person:"-TheKnownOne"
+},
+{
+    quote:"Love more than being loved." ,
+    person:"TheKnownOne"
+},
+{
+    quote:"Tell me and I forget. Teach me and I remember. Involve me and I learn." ,
+    person:"Benjamin Franklin"
+},
+{
+    quote:"It is during our darkest moments that we must focus to see the light." ,
+    person:"-Aristotle"
+},
+{
+    quote:"Whoever is happy will make others happy too." ,
+    person:"Anne Frank"
+},
+{
+    quote:"The only impossible journey is the one you never begin." ,
+    person:"Tony Robbins"
+},
+{
+    quote:"Only a life lived for others is a life worthwhile" ,
+    person:"Einstein"
+},
+{
+    quote:"May you live all the days of your life." ,
+    person:"Jonathan Swift"
+},
+{
+    quote:"Love , because before than anything else love is something else." ,
+    person:"TheKnownOne"
+},
+];
+
+btn.addEventListener("click", function(){
+
+    let random = Math.floor(Math.random() * quotes.length);
+
+    quote.innerText = quotes[random].quote;
+    person.innerText = quotes[random].person;
+
+})
